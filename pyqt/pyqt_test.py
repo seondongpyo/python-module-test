8120cc527d3ca4a7ef05604c59737051b28b3891
seondongpyo <psd1390@gmail.com>
2021-01-30 18:30:14 +0900

    버튼 추가 테스트

--- pyqt/pyqt_test.py
+++ pyqt/pyqt_test.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
 
 class TestApp(QWidget):
@@ -10,10 +11,18 @@ class TestApp(QWidget):
         self.initialize_ui()
 
     def initialize_ui(self):
+        self.add_quit_button()  # 종료 버튼 추가
+
         self.setWindowTitle('Test Application')  # 타이틀 설정
         self.move(300, 300)  # 위치 지정
         self.resize(400, 400)  # 크기 지정
         self.show()  # 화면 표시
+
+    def add_quit_button(self):
+        btn = QPushButton('종료', self)  # QPushButton('버튼에 표시될 텍스트', '버튼이 위치할 부모 위젯')
+        btn.move(150, 150)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)  # 클릭 시 어플리케이션을 종료하도록 이벤트 설정
 
 
 if __name__ == '__main__':
