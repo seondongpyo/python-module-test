613f95a3a64190402797c58129dbd786b5347007
seondongpyo <psd1390@gmail.com>
2021-02-06 22:51:00 +0900

    기존 PDF로 새로운 PDF 파일 생성하기

--- pypdf2/pypdf2_test.py
+++ pypdf2/pypdf2_test.py
@@ -1,6 +1,6 @@
 import os
 
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 # PyPDF2 documentation : https://pythonhosted.org/PyPDF2/index.html
 
@@ -23,4 +23,11 @@ author = pdf_info.author
 first_page = pdf.getPage(0)
 first_page_text = first_page.extractText()
 
+# 첫 페이지의 내용으로 새로운 PDF 파일 생성하기
+pdf_writer = PdfFileWriter()
+pdf_writer.addPage(first_page)
+new_pdf_file_name = 'new.pdf'
+new_pdf_file_path = os.path.join(os.getcwd(), new_pdf_file_name)
 
+with open(new_pdf_file_path, 'wb') as new_pdf_file:
+    pdf_writer.write(new_pdf_file)
